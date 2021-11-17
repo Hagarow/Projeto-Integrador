346f2d52df4224401a0ccc3c98afd8f9aa5fc3e1
Jonatam Luiz Emidio <jonatan.luiz.je@gmail.com>
2021-11-17 20:11:35 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Integrador/usuarios.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace Projeto_Integrador
+{
+    partial class usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbnome = new System.Windows.Forms.TextBox();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnexcluir = new System.Windows.Forms.Button();
+            this.btnlistar = new System.Windows.Forms.Button();
+            this.tbpesquisarnome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbcodigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbemail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbsenha = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbtelefone = new System.Windows.Forms.MaskedTextBox();
+            this.tbcelular = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.Location = new System.Drawing.Point(86, 36);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btncadastrar.TabIndex = 0;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // tbnome
+            // 
+            this.tbnome.Location = new System.Drawing.Point(124, 193);
+            this.tbnome.Name = "tbnome";
+            this.tbnome.Size = new System.Drawing.Size(317, 20);
+            this.tbnome.TabIndex = 2;
+            // 
+            // btneditar
+            // 
+            this.btneditar.Location = new System.Drawing.Point(182, 36);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(75, 23);
+            this.btneditar.TabIndex = 3;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btnexcluir
+            // 
+            this.btnexcluir.Location = new System.Drawing.Point(366, 36);
+            this.btnexcluir.Name = "btnexcluir";
+            this.btnexcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnexcluir.TabIndex = 4;
+            this.btnexcluir.Text = "Excluir";
+            this.btnexcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnlistar
+            // 
+            this.btnlistar.Location = new System.Drawing.Point(276, 36);
+            this.btnlistar.Name = "btnlistar";
+            this.btnlistar.Size = new System.Drawing.Size(75, 23);
+            this.btnlistar.TabIndex = 5;
+            this.btnlistar.Text = "Listar";
+            this.btnlistar.UseVisualStyleBackColor = true;
+            // 
+            // tbpesquisarnome
+            // 
+            this.tbpesquisarnome.Location = new System.Drawing.Point(171, 96);
+            this.tbpesquisarnome.Name = "tbpesquisarnome";
+            this.tbpesquisarnome.Size = new System.Drawing.Size(270, 20);
+            this.tbpesquisarnome.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pesquisar nome";
+            // 
+            // tbcodigo
+            // 
+            this.tbcodigo.Location = new System.Drawing.Point(124, 132);
+            this.tbcodigo.Name = "tbcodigo";
+            this.tbcodigo.Size = new System.Drawing.Size(81, 20);
+            this.tbcodigo.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Código";
+            // 
+            // tbemail
+            // 
+            this.tbemail.Location = new System.Drawing.Point(124, 232);
+            this.tbemail.Name = "tbemail";
+            this.tbemail.Size = new System.Drawing.Size(317, 20);
+            this.tbemail.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "E-mail";
+            // 
+            // tbsenha
+            // 
+            this.tbsenha.Location = new System.Drawing.Point(124, 268);
+            this.tbsenha.Name = "tbsenha";
+            this.tbsenha.Size = new System.Drawing.Size(81, 20);
+            this.tbsenha.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Senha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(83, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Telefone";
+            // 
+            // tbtelefone
+            // 
+            this.tbtelefone.Location = new System.Drawing.Point(138, 163);
+            this.tbtelefone.Mask = "(00)00000-0000";
+            this.tbtelefone.Name = "tbtelefone";
+            this.tbtelefone.Size = new System.Drawing.Size(100, 20);
+            this.tbtelefone.TabIndex = 15;
+            // 
+            // tbcelular
+            // 
+            this.tbcelular.Location = new System.Drawing.Point(341, 163);
+            this.tbcelular.Mask = "(00)00000-0000";
+            this.tbcelular.Name = "tbcelular";
+            this.tbcelular.Size = new System.Drawing.Size(100, 20);
+            this.tbcelular.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(296, 166);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Celular";
+            // 
+            // usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbcelular);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbtelefone);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbsenha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbemail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbcodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbpesquisarnome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnlistar);
+            this.Controls.Add(this.btnexcluir);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.tbnome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btncadastrar);
+            this.Name = "usuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "usuarios";
+            this.Load += new System.EventHandler(this.usuarios_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbnome;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnexcluir;
+        private System.Windows.Forms.Button btnlistar;
+        private System.Windows.Forms.TextBox tbpesquisarnome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbcodigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbemail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbsenha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox tbtelefone;
+        private System.Windows.Forms.MaskedTextBox tbcelular;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Projeto Integrador/usuarios.cs
@@ -0,0 +1,87 @@
+﻿
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Projeto_Integrador
+{
+    public partial class usuarios : Form
+    {
+        public usuarios()
+        {
+            InitializeComponent();
+
+            tbcodigo.Enabled = true;
+            tbtelefone.Enabled = true;
+            tbcelular.Enabled = true;
+            tbnome.Enabled = true;
+            tbemail.Enabled = true;
+            tbsenha.Enabled = true;
+            tbpesquisarnome.Enabled = true;
+        }
+
+        private MySqlConnectionStringBuilder ConexaoBanco()
+        {
+            MySqlBaseConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "locadora";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = MySqlSslMode.None;
+            return (MySqlConnectionStringBuilder)conexaoBD;
+        }
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = ConexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                //INSERT INTO usuarios (codUsuario,telefoneUsuario,celularUsuario,nomeUsuario,emailUsuario,senhaUsuario)
+                //VALUES('" + tbcodigo.Text + "', '" + tbtelefone.Text + "', '" + tbcelular.Text + "', '" + tbnome.Text + "', '" + tbemail.Text + "', '" + tbsenha.Text + "')
+
+                comandoMySql.CommandText = "INSERT INTO usuarios (codUsuario,telefoneUsuario,celularUsuario,nomeUsuario,emailUsuario,senhaUsuario)" +
+                    "VALUES('" + tbcodigo.Text + "', '" + tbtelefone.Text + "', '" + tbcelular.Text + "', '" + tbnome.Text + "', '" + tbemail.Text + "', '" + tbsenha.Text + "')";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("iTEM INSERIDO COM SUCESSO!");
+                // AtualizaGrid();
+                LimparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void usuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimparCampos()
+        {
+            tbcodigo.Clear();
+            tbtelefone.Clear();
+            tbcelular.Clear();
+            tbnome.Clear();
+            tbemail.Clear();
+            tbsenha.Clear();
+        }
+    }
+}
